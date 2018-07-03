b5c07c0a1e9e983779a18d7bd1bc8fad995de554
udass <anurag00521202016@msi-ggsip.org>
2018-07-03 16:09:12 +0530

    php file to connect to database

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$link = mysqli_connect("localhost","geeksthe_notes","Anurag123$","geeksthe_onlinenotes");
+if(mysqli_connect_error()){
+    die("Error: Unable to connect".mysqli_connect_error());
+    
+}
+
+
+
+?>
