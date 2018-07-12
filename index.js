e6731522e5a02d10670c66fac5a93243a0478317
Anurag <anuragmy2729@gmail.com>
2018-07-13 02:06:19 +0530

    updated js file with new ajax calls

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//ajax call to signup.php
+
 $("#signupform").submit(function(event){
     //prevent default processing of php
     
@@ -22,3 +24,85 @@ $("#signupform").submit(function(event){
     });
     
 });
+
+//ajax call tologin.php
+
+$("#loginform").submit(function(event){
+    //prevent default processing of php
+    event.preventDefault();
+    
+    //collect user input
+    var datatopost = $(this).serializeArray();
+    //send to signup.php using ajax
+    
+    $.ajax({
+       url:"login.php",
+       type:"POST",
+       data:datatopost,
+       success: function(data) {
+           if(data == "success") {
+               window.location="loggedpage.php";
+           }
+           else {
+               $("#loginmsg").html(data);
+           }
+       },
+       error: function() {
+           $("#loginmsg").html("<div class='alert alert-danger'>Problem in ajax call <button class='close' data-dismiss='alert'>&times;</div>");
+       }
+    });
+    
+});
+
+//ajax call to forgot password
+$("#forgotpass").submit(function(event){
+    //prevent default processing of php
+    event.preventDefault();
+    
+    //collect user input
+    var datatopost = $(this).serializeArray();
+    //send to signup.php using ajax
+    
+    $.ajax({
+       url:"forgot_password.php",
+       type:"POST",
+       data:datatopost,
+       success: function(data) {
+           if(data == "success") {
+               window.location="reserpassword.php";
+           }
+           else {
+               $("#forgotpassmsg").html(data);
+           }
+       },
+       error: function() {
+           $("#forgotpassmsg").html("<div class='alert alert-danger'>Problem in ajax call <button class='close' data-dismiss='alert'>&times;</div>");
+       }
+    });
+    
+});
+
+
+//ajax call to reste password
+$("#passwordreset").submit(function(event){
+    //prevent default processing of php
+    event.preventDefault();
+    
+    //collect user input
+    var datatopost = $(this).serializeArray();
+    //send to signup.php using ajax
+    
+    $.ajax({
+       url:"storeresetpassword.php",
+       type:"POST",
+       data:datatopost,
+       success: function(data) {
+            $("#resetpassmsg").html(data);
+       },
+       error: function() {
+           $("#resetpassmsg").html("<div class='alert alert-danger'>Problem in ajax call <button class='close' data-dismiss='alert'>&times;</div>");
+       }
+    });
+    
+});
+
